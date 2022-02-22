484f8c5244f70c9a630e6ea59d057d5849685560
Parag-A-Sethiya <sethiyaparag@gmail.com>
2022-02-22 15:44:48 +0530

    uploading

---
+++ Parag.java
@@ -0,0 +1,12 @@
+
+ public class Parag
+  {
+    public static void main(String a[])
+    {
+    	char c;
+    	c='1';
+     System.out.println("helllo parag!");
+     System.out.println("char="+c);
+    }
+
+  }
